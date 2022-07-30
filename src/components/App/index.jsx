23a3ec3fa6f1171cf7c4b8c00f5ed8bb435d6282
Vlad Koblay <vkchabraww@gmail.com>
2022-07-30 14:57:10 +0300

    hooks done

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import styles from './app.module.css';
 import ContactForm from 'components/ContactForm';
@@ -7,18 +7,19 @@ import Contacts from 'components/Contacts';
 
 const LS_SAVEDCONTACTS = 'Contacts';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem(LS_SAVEDCONTACTS)) ?? [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
+  const [filter, setFilter] = useState('');
 
-  submitAddContact = e => {
+  const submitAddContact = e => {
     e.preventDefault();
     const form = e.target;
     const name = form.elements.name.value;
@@ -26,7 +27,7 @@ export class App extends Component {
     form.reset();
 
     const normalizedName = name.toLowerCase();
-    const existingName = this.state.contacts.find(contact =>
+    const existingName = contacts.find(contact =>
       contact.name.toLowerCase().includes(normalizedName)
     );
 
@@ -40,54 +41,32 @@ export class App extends Component {
       number,
     };
 
-    this.setState(prevState => ({
-      contacts: [contact, ...prevState.contacts],
-    }));
+    setContacts([contact, ...contacts]);
   };
 
-  onChangeFilter = e => {
-    this.setState({ filter: e.target.value });
+  const onChangeFilter = e => {
+    setFilter(e.target.value);
   };
 
-  deleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const deleteContact = id => {
+    setContacts(contacts.filter(contact => contact.id !== id));
   };
 
-  componentDidMount() {
-    const savedContacts = JSON.parse(localStorage.getItem(LS_SAVEDCONTACTS));
-    if (savedContacts) {
-      this.setState({ contacts: savedContacts });
-    }
-  }
+  useEffect(() => {
+    window.localStorage.setItem(LS_SAVEDCONTACTS, JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate(_, prevState) {
-    const contactsList = this.state.contacts;
-    if (prevState.contacts !== contactsList) {
-      localStorage.setItem(LS_SAVEDCONTACTS, JSON.stringify(contactsList));
-    }
-  }
-
-  render() {
-    const { contacts, filter } = this.state;
-
-    const normalizedFilter = filter.toLowerCase();
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-
-    return (
-      <div className={styles.wrapper}>
-        <h1>Phonebook</h1>
-        <ContactForm submitAddContact={this.submitAddContact} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChangeFilter={this.onChangeFilter} />
-        <Contacts
-          contacts={filteredContacts}
-          deleteContact={this.deleteContact}
-        />
-      </div>
-    );
-  }
-}
+  const normalizedFilter = filter.toLowerCase();
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+  return (
+    <div className={styles.wrapper}>
+      <h1>Phonebook</h1>
+      <ContactForm submitAddContact={submitAddContact} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChangeFilter={onChangeFilter} />
+      <Contacts contacts={filteredContacts} deleteContact={deleteContact} />
+    </div>
+  );
+};
